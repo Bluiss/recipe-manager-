87d611afdac386a63ac390d14fc0a6cb499bc9c5
Bluiss <135335614+Bluiss@users.noreply.github.com>
2023-10-26 22:40:17 +0100

    add recipe search function based on cuisine with print statements

--- run.py
+++ run.py
@@ -1,10 +1,33 @@
 import gspread
 
+
 sa = gspread.service_account(filename='creds.json')
 sh = sa.open("recipe_manager")
+worksheet = sh.worksheet('Sheet1')
+data = worksheet.get_all_records(default_blank=True)
 
-wks = sh.worksheet('Sheet1')
 
 
-print('Rows: ',wks.row_count)
-print('Cols: ',wks.col_count)
+def recipeSearch():
+    """
+    Search for a recipe based on the cuisine
+    """
+    while True:
+        print("What Cuisine would you like to eat?\n")
+
+        search_value = input("Enter your Cuisine here:\n")
+
+        if search_value.lower() == 'exit':
+            break
+
+        matching_rows = [row for row in data if row["Cuisine"] == search_value]
+        
+        if not matching_rows:
+            print("Try again, no cuisine matches that input")
+        else:
+            print("Here is all the", search_value, "food we have")
+            for row in matching_rows:
+                print(row)
+
+
+recipeSearch()
