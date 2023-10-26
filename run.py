7646232a6722e42992146a5c2ec60d48a6032c40
Bluiss <135335614+Bluiss@users.noreply.github.com>
2023-10-26 21:01:51 +0100

    initial commit, connect recipe sheets to run.py

--- run.py
+++ run.py
@@ -1,3 +1,10 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+
+sa = gspread.service_account(filename='creds.json')
+sh = sa.open("recipe_manager")
+
+wks = sh.worksheet('Sheet1')
+
+
+print('Rows: ',wks.row_count)
+print('Cols: ',wks.col_count)
