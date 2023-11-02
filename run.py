6f3c5f55e7ed69ac06f0cf48a8141e37e122fc14
Bluiss <135335614+Bluiss@users.noreply.github.com>
2023-11-02 19:28:50 +0000

    add the create recipe function

--- run.py
+++ run.py
@@ -10,9 +10,10 @@ worksheet = sh.worksheet('Sheet1')
 data = worksheet.get_all_records(default_blank=True)
 
 
+
 def print_pause(message):
     print(message)
-    time.sleep(2)
+    time.sleep(1)
 
 def startManager():
     """
@@ -21,10 +22,10 @@ def startManager():
     while True:
         startAnswer = input("what would you like to do?\n (1)Create a recipe?\n (2)Search for a Recipe?\n (3)Meal plan a week?\n")
         if startAnswer.lower() == "1":
-            print("create")
+            addRecipe()
             break
         elif startAnswer.lower() == "2":
-            selected_column = input("Enter your choice (or type 'exit' to quit): ")
+            selected_column = input("")
             recipeSearch(data, selected_column)
             break
         elif startAnswer.lower() == "3":
@@ -33,17 +34,30 @@ def startManager():
         else:
             print("Opps, try again either 1,2 or 3")
 
+def addRecipe():
+    name = input("Please Enter the recipes name: ").strip()
+    ingredients = input("Please Enter the ringredients: ").strip()
+    instructions = input("Enter in the recipes instructions : ").strip()
+    time = input("Total time it takes to cook : ").strip()
+    servings = input("Total Servings : ").strip()
+    cuisine = input("What cuisine is it? : ").strip()
+    dietaryRestrictions = input("Any Dietary restrictions? : ").strip()
+    rating =  input("Finally, how good is it. Give it a rating out of 5!: ").strip()
+
+
+    values = [[name, ingredients, instructions, time, servings, cuisine, dietaryRestrictions, rating]]
+    worksheet.insert_rows(values, row=42)
+    print("Recipe added successfully!")
 
 
 
-
-def recipeSearch(data, column_name, search_value):
-    matching_rows = [row for row in data if row.get(column_name) == search_value]
+def recipeSearch(data, column_name):
+    matching_rows = [row for row in data if row.get(column_name) == {column_name}]
 
     if not matching_rows:
-        print(f"Sorry, there's nothing in our database under {column_name} based on {search_value}")
+        print(f"Sorry, there's nothing in our database under {column_name} based on {column_name}")
     else:
-        print(f"Please see all recipes associated with {search_value}")
+        print(f"Please see all recipes associated with {column_name}")
         for row in matching_rows:
             print(row)
 
@@ -59,8 +73,8 @@ def recipeSearch(data, column_name, search_value):
 
 
 def main():
-    print_pause("Weclome to the Recpie Manager!")
-    print_pause("we have thousands of recpies you can look into ")
+    print_pause("Weclome to the Recipe Manager! ")
+    print_pause("We have hundreds of recipes you can look into ")
     startManager()
 
 main()
